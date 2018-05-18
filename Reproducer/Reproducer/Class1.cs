b917595fd8695a38bedde4548c169f5bfb568a85
mishrap <mishrap@synopsys.com>
2018-05-18 10:19:28 -0400

    first commeit

---
+++ Reproducer/Reproducer/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Reproducer
+{
+    public class Class1
+    {
+
+        private Object TestFunction(Object newValue)
+        {
+            if(newValue == null)
+            {
+                throw new Library.MyException();
+            }
+
+            return newValue.ToString();
+        }
+    }
+}
